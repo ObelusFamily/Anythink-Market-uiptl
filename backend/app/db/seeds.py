96eb66448e5f445cfaf64b491f9cd68eb3272c0f
Sriza Sen <sriza20124@iiitd.ac.in>
2022-09-17 13:28:02 +0530

    Update seeds.py

--- backend/app/db/seeds.py
+++ backend/app/db/seeds.py
@@ -2,32 +2,45 @@
 #add 100 users, 100 comments, 100 products
 import string
 import random
+import asyncpg
 from events import connect_to_db
 from app.db.repositories.items import ItemsRepository
 from app.db.repositories.comments import CommentsRepository
 from app.db.repositories.users import UsersRepository
 
+from app.core.config import get_app_settings
+
+SETTINGS = get_app_settings()
+DATABASE_URL = SETTINGS.database_url.replace("postgres://", "postgresql://")
+
+
 def random_char(char_num):
        return ''.join(random.choice(string.ascii_letters) for _ in range(char_num))
+           
 
-usersList=[]
-for i in range(0,100):
-    username=''.join(random.choices(string.ascii_lowercase)
-    email=random_char(9)+"@gmail.com"
-    password=''.join(random.choices(string.ascii_letters +string.digits))
-    us=UsersRepository.create_user(self,username,email,password)
-    usersList.append(us)
+async def create():
+    conn = await asyncpg.connect(DATABASE_URL)
+    usersRepository = UsersRepository(conn=conn)
+    usersList=[]
+    for i in range(0,100):
+        username=''.join(random.choices(string.ascii_lowercase))
+        email=random_char(9)+"@gmail.com"
+        password=''.join(random.choices(string.ascii_letters +string.digits))
+        #us=UsersRepository.create_user(self,username,email,password)
+        #usersList.append(us)
+        user = await usersRepository.create_user(username=username, password=password, email=email)
+        usersList.append(user)
+    
+    itemList=[]
+    for i in range(0,100):
+        slug=''.join(random.choices(string.ascii_letters +string.digits))
+        title=''.join(random.choices(string.ascii_letters +string.digits))
+        description=''.join(random.choices(string.ascii_letters +string.digits))
+        seller=usersList[i]
 
-itemList=[]
-for i in range(0,100):
-    slug=''.join(random.choices(string.ascii_letters +string.digits))
-    title=''.join(random.choices(string.ascii_letters +string.digits))
-    description=''.join(random.choices(string.ascii_letters +string.digits))
-    seller=usersList[i]
+        item=await ItemsRepository.create_item(slug=slug,title=title,description=description,seller=seller)
+        itemList.append(item)
 
-    item=ItemsRepository.create_item(self,slug,title,description,seller)
-    itemList.append(item)
-
-for i in range(0,100):
-    body=''.join(random.choices(string.ascii_letters +string.digits))
-    CommentsRepository.create_comment_for_item(body,itemList[i],usersList[i])
+    for i in range(0,100):
+        body=''.join(random.choices(string.ascii_letters +string.digits))
+        comment= await CommentsRepository.create_comment_for_item(body=body,item=itemList[i],user=usersList[i])
